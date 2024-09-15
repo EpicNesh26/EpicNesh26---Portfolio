69063a2dde3fa5e8a9b47303c67986d568de8b77
Nesh <soninesh2006@gmail.com>
2024-09-14 18:36:40 -0600

    Completed Responsiveness For Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        '2xl.5':'1600px',
+        '2xl.6':'1700px',
+        '2xl.7':'1800px',
+        '3xl': '1920px',
+        '4xl': '2560px',
+        '5xl': '3840px'
+      },
       backgroundImage: {
         'custom-background': "url('/images/BACKGROUND.jpg')",
-        
+
       },
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
